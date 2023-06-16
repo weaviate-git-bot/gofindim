de3c85d5c67cdda80de9e901e1b6d6f572809f71
agentx3 <agentx3@thepuzzlegang.agent@gmail.com>
2023-06-16 14:36:09 -0700

    Modified ParseImageData fn

--- data/weaviate_data.go
+++ data/weaviate_data.go
@@ -13,30 +13,39 @@ func ParseImageData(data map[string]models.JSONObject, fields []string) ([]Image
 	if len(data) == 0 {
 		return results, nil
 	}
-	outerValue := data["Get"]
-	// println(data["Get"].(map[string]interface{})["Image"].([]interface{})[0].(map[string]interface{})["path"])
-	if outerMap, ok := outerValue.(map[string]interface{}); !ok {
-		return nil, fmt.Errorf("error parsing outerValue: %v", outerValue)
-	} else if images, ok := outerMap["Image"].([]interface{}); !ok {
-		return nil, fmt.Errorf("error parsing outerMap: %v", outerMap)
-	} else {
-		for _, image := range images {
-			if img, ok := image.(map[string]interface{}); !ok {
-				return results, nil
-			} else {
-				node := &ImageNode{}
-				nodeValue := reflect.ValueOf(node).Elem()
-				nodeType := reflect.TypeOf(ImageNode{})
-				for i := 0; i < nodeValue.NumField(); i++ {
-					field := nodeType.Field(i).Name
-					key := strings.ToLower(field)
-					if value, ok := img[key]; ok {
-						nodeValue.Field(i).Set(reflect.ValueOf(value))
-					}
+
+	outerValue, ok := data["Get"].(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("error parsing outerValue: expected map[string]interface{}, but got %T", data["Get"])
+	}
+
+	images, ok := outerValue["Image"].([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("error parsing outerMap: expected []interface{}, but got %T", outerValue["Image"])
+	}
+
+	for _, image := range images {
+		img, ok := image.(map[string]interface{})
+		if !ok {
+			return results, nil
+		}
+		node := &ImageNode{}
+		nodeValue := reflect.ValueOf(node).Elem()
+		nodeType := reflect.TypeOf(ImageNode{})
+
+		for i := 0; i < nodeValue.NumField(); i++ {
+			field := nodeType.Field(i).Name
+			key := strings.ToLower(field)
+
+			if key == "id" {
+				if id, ok := img["_additional"].(map[string]interface{})["id"].(string); ok {
+					nodeValue.Field(i).Set(reflect.ValueOf(id))
 				}
-				results = append(results, *node)
+			} else if value, ok := img[key]; ok {
+				nodeValue.Field(i).Set(reflect.ValueOf(value))
 			}
 		}
+		results = append(results, *node)
 	}
 
 	return results, nil
